5987afa32043b69ba599d9b043b711c1e9aea2c7
xazhuxj <xazhuxj@hotmail.com>
2020-04-28 17:05:54 +0800

    1.7 修改了界面表达部分，将数据用RecylerView进行表达，增加了CardView

--- app/src/main/java/com/xazhuxj/words/MainActivity.java
+++ app/src/main/java/com/xazhuxj/words/MainActivity.java
@@ -3,56 +3,91 @@ package com.xazhuxj.words;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-//import androidx.lifecycle.ViewModelProviders; //不再使用
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     WordViewModel wordViewModel;
+    RecyclerView recyclerView;
+    CellAdapter cellNormalAdapter, cellCardAdapter;
+    Switch aSwitch;
 
-    private Button buttonInsert, buttonUpdate, buttonClear, buttonDelete;
+    private Button buttonInsert, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-//        wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class); //不再使用
+        aSwitch = findViewById(R.id.switch1);
+        cellNormalAdapter = new CellAdapter(false);
+        cellCardAdapter = new CellAdapter(true);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(cellNormalAdapter);
+        aSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                recyclerView.setAdapter(cellCardAdapter);
+            }else {
+                recyclerView.setAdapter(cellNormalAdapter);
+            }
+        });
+
+
         wordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
         wordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(List<Word> words) {
-                StringBuilder text = new StringBuilder();
-                for (int i = 0; i < words.size(); i++) {
-                    Word word = words.get(i);
-                    text.append(word.getId()).append(":").append(word.getWord()).append("=").append(word.getChineseMeaning() + "\n");
-                }
-                textView.setText(text.toString());
+                cellNormalAdapter.setAllWords(words);
+                cellNormalAdapter.notifyDataSetChanged();
+                cellCardAdapter.setAllWords(words);
+                cellCardAdapter.notifyDataSetChanged();
             }
         });
+
         buttonInsert = findViewById(R.id.buttonInsert);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Word word1 = new Word("Hello", "你好");
-                Word word2 = new Word("World", "世界");
-                wordViewModel.insertWords(word1, word2);
-            }
-        });
-
-        buttonUpdate = findViewById(R.id.buttonUpdate);
-        buttonUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Word word = new Word("Hi", "你好啊");
-                word.setId(140);
-                wordViewModel.updateWords(word);
+                String[] english = {
+                        "Hello",
+                        "World",
+                        "Android",
+                        "Google",
+                        "Studio",
+                        "Project",
+                        "Database",
+                        "Recycler",
+                        "View",
+                        "String",
+                        "Value",
+                        "Integer"
+                };
+                String[] chinese = {
+                        "你好",
+                        "世界",
+                        "安卓系统",
+                        "谷歌公司",
+                        "工作室",
+                        "项目",
+                        "数据库",
+                        "回收站",
+                        "视图",
+                        "字符串",
+                        "价值",
+                        "整数类型"
+                };
+                for(int i=0; i < english.length; i++) {
+                    wordViewModel.insertWords(new Word(english[i], chinese[i]));
+                }
             }
         });
 
@@ -61,16 +96,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 wordViewModel.deleteAllWords();
-            }
-        });
-
-        buttonDelete = findViewById(R.id.buttonDelete);
-        buttonDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Word word = new Word("Hi", "你好啊");
-                word.setId(140);
-                wordViewModel.deleteWords(word);
             }
         });
     }
